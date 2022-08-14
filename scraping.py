2b88b685c1e9879cd38aa723480c05f06c1f17f3
Marianne Valdespino <mariannevaldespino@Mariannes-MacBook-Pro.local>
2022-08-14 17:20:42 -0400

    updating files

--- scraping.py
+++ scraping.py
@@ -3,12 +3,12 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 import datetime as dt
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def scrape_all():
     # Initiate headless driver for deployment
-    #executable_path = {'executable_path': ChromeDriverManager().install()}
+    executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
